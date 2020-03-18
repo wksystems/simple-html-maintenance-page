e3efffb6db1614a66d16fe71f76434f24078178d
WKS <33673467+wksystems@users.noreply.github.com>
2020-03-18 19:06:38 -0400

    Update install.sh
    added a new comment

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# just added a comment HM
 TEMP_FILE=$(mktemp /tmp/maintenance_index.html.XXXXX)
 
 curl -sSL https://raw.githubusercontent.com/itconsultis/maintenance/master/index.html > $TEMP_FILE
